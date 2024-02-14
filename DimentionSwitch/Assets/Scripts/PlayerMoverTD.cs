6c2f466fe3942730075adde51c21dba5c86db6b6
Suhas Aditya SM <suhasadityasm@gmail.com>
2024-02-14 16:45:31 +0530

    done with majority top down

---
+++ DimentionSwitch/Assets/Scripts/PlayerMoverTD.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMoverTD : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    Vector2 Movement;
+    Vector2 Movedir;
+    Vector2 LastMovedir;
+    Vector2 Rolldir;
+    public float MoveSpeed = 5f;
+    public Animator anim;
+    public float RollSpeed;
+    private enum State
+    {
+        Normal,
+        Rolling,
+        Attack,
+    }
+    private State state;
+    private void Awake()
+    {
+        state = State.Normal;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = State.Normal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Normal:
+                Movement.x = Input.GetAxisRaw("Horizontal");
+                Movement.y = Input.GetAxisRaw("Vertical");
+                if ((Movement.x == 0 && Movement.y == 0) && Movedir.x != 0 || Movedir.y != 0)
+                {
+                    LastMovedir = Movedir;
+                }
+                Movedir = new Vector2(Movement.x, Movement.y).normalized;
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    Rolldir = Movedir;
+                    RollSpeed = 20f;
+                    state= State.Rolling;
+                }
+                break;
+            case State.Rolling:
+                float RollSpeedDropMul = 5f;
+                RollSpeed -= RollSpeed * RollSpeedDropMul * Time.deltaTime;
+                float RollSpeedMin = 5f;
+                if (RollSpeed < RollSpeedMin)
+                {
+                    state = State.Normal;
+                }
+                break;
+        }
+
+
+        UpdateAnimations();
+    }
+    private void FixedUpdate()
+    {
+        switch(state)
+        {
+            case State.Normal:
+                rb.MovePosition(rb.position + Movement * MoveSpeed * Time.fixedDeltaTime);
+                break;
+            case State.Rolling:
+                DodgeRoll();
+                break;
+        }
+    }
+
+    private void DodgeRoll()
+    {
+        rb.velocity = Rolldir * RollSpeed;
+    }
+
+    private void UpdateAnimations()
+    {
+        anim.SetFloat("xdirection", Movedir.x);
+        anim.SetFloat("ydirection", Movedir.y);
+        anim.SetFloat("xLastMoveDir", LastMovedir.x);
+        anim.SetFloat("yLastMoveDir", LastMovedir.y);
+        anim.SetFloat("RunMagnitude", Movedir.magnitude);
+        anim.SetFloat("RollMagnitude",RollSpeed);
+    }
+}
